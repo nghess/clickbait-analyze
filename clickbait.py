330eb1914b71de3d28365313cf80fec50af50a78
Nate Gonzales-Hess <nhess@uoregon.edu>
2024-11-07 21:14:36 -0800

    plotting

--- clickbait.py
+++ clickbait.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+from numpy import isin
 import plotly.graph_objects as go
 import plotly.colors as colors
 import plotly.io as pio
@@ -37,22 +38,28 @@ def scan_dataset(data_directory, min_size_bytes, filetype='.tif'):
 '''
 Visualization
 '''
-def visualize_trial(df, trial_number, color_code="trial_number", dim_x=894, dim_y=1952, opacity=1.0):
+def visualize_trial(df, trial_number, color_code="trial_number", grid_x=4, grid_y=9, dim_x=894, dim_y=1952, opacity=1.0):
+    assert color_code in(['trial_number', 'frame_number', 'velocity']), "Valid color codes are 'trial_number', 'frame_number', and 'velocity'."
+    
     df = df.loc[df['trial_number'].isin(trial_number)].copy()
-    df[color_code] = df[color_code] - df[color_code].min() # Start counting frames from 0
+
+    if color_code == "frame_number":
+            df = df.reset_index(drop=True)
+            color_code = df.index
 
     # Create the scatter plot with Plotly
-    fig = px.scatter(df, x='centroid_x', y='centroid_y', color=color_code, opacity=opacity, title=f"2D Scatter Plot of Centroid Coordinates<br><b>Entire Session</b>")
-
-    # Center the plot at (0, 0)
+    fig = px.scatter(df, x='centroid_x', y='centroid_y', color=color_code, opacity=opacity, color_continuous_scale="Bluered",
+                    title=f"2D Scatter Plot of Centroid Coordinates<br><b>Trials: {str(trial_number)[1:-1]}</b>")
+    
     fig.update_layout(
-        xaxis=dict(range=[0, dim_x], zeroline=False, zerolinewidth=2, zerolinecolor='LightPink'),
-        yaxis=dict(range=[0, dim_y], zeroline=False, zerolinewidth=2, zerolinecolor='LightPink'),
+        xaxis=dict(tick0=0, dtick=dim_x//grid_x, gridwidth=1),
+        yaxis=dict(tick0=0, dtick=dim_y//grid_y, gridwidth=1),
         xaxis_title='X Coordinate',
         yaxis_title='Y Coordinate',
         yaxis_scaleanchor='x',
         width=dim_x,
-        height=dim_y)
+        height=dim_y
+        )
 
     # Show the plot
     fig.show()
